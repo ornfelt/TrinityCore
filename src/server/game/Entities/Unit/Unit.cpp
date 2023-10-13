e3d87b93d7652ac2e53f453ddaa506d3b26ab099
ornfelt <jonasornfelt@outlook.com>
2023-10-13 07:41:10 +0200

    Update worldserver config with base delay to npcbots. Format fix for sql file and server-side buff CoC :)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -1152,8 +1152,8 @@ void Unit::CalculateSpellDamageTaken(SpellNonMeleeDamage* damageInfo, int32 dama
     sScriptMgr->ModifySpellDamageTaken(damageInfo->target, damageInfo->attacker, damage);
 
     // Ornfelt: Buff spells
-    // Buff lava burst
-    if (spellInfo->Id == 60043)
+    // Buff lava burst and CoC
+    if (spellInfo->Id == 60043 || spellInfo->Id == 46984)
         damage *= 2;
     // Buff earth shock
     else if (spellInfo->Id == 49231)
